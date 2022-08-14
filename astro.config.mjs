76c1bccd98b7e2547752f58f1994ed44fb5fed39
GDWR <57012020+GDWR@users.noreply.github.com>
2022-08-14 15:16:57 +0000

    Add plugins and set codeblock colour

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,21 @@
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
 import sitemap from '@astrojs/sitemap';
-
+import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [sitemap(), tailwind(), mdx()]
+  integrations: [
+    sitemap(),
+    tailwind(), 
+    mdx()
+  ],
+  markdown: {
+    remarkPlugins: ['remark-gfm', 'remark-smartypants'],
+    rehypePlugins: ['rehype-sanitize', 'rehype-autolink-headings', 'rehype-slug'],
+    shikiConfig: {
+      theme: 'slack-dark',
+      wrap: true,
+    },
+  },
 });
