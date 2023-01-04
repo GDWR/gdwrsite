b44f1e58a8e7d7fb5bb4d24b50635bbe29e5d92c
GDWR <gregory.dwr@gmail.com>
2023-01-04 11:35:01 +0000

    Create tailwind prose GDWR theme

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,18 @@
-import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://gdwr.me',
+  site: "https://gdwr.me",
   integrations: [sitemap(), tailwind(), mdx()],
   markdown: {
-    remarkPlugins: ['remark-gfm', 'remark-smartypants'],
-    rehypePlugins: ['rehype-autolink-headings', 'rehype-slug'],
+    remarkPlugins: ["remark-gfm", "remark-smartypants"],
+    rehypePlugins: ["rehype-autolink-headings", "rehype-slug"],
     shikiConfig: {
-      theme: 'slack-dark',
-      wrap: true
-    }
-  }
+      theme: "slack-dark",
+      wrap: true,
+    },
+  },
 });
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,15 @@ module.exports = {
     './src/**/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      typography: ({ theme }) => ({
+        gdwr: {
+          css: {
+            '--tw-prose-invert-links': theme('colors.orange[300]'),
+          },
+        },
+      }),
+    },
   },
   plugins: [
     require('@tailwindcss/typography'),
